acd44085aaaf219de3dd36939dd75f54c6ec0fbb
Elisa Jahan <elisa.jahan@free.fr>
2021-04-21 12:17:49 +0200

    6

--- Dockerfile
+++ Dockerfile
@@ -6,28 +6,22 @@
 #    By: ejahan <ejahan@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/17 15:47:25 by ejahan            #+#    #+#              #
-#    Updated: 2021/04/19 11:37:07 by ejahan           ###   ########.fr        #
+#    Updated: 2021/04/21 12:15:39 by ejahan           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 FROM debian:buster-slim
 
 RUN apt-get -y update && apt-get -y install mariadb-server \
-            wget \
-            php \
-            php-cli \
-            php-cgi \
-            php-mbstring \
-            php-fpm \
-            php-mysql \
-            nginx \
-            libnss3-tools
+			wget \
+			nginx \
+			openssl
 
 
 COPY ./srcs/ ./root/
 
 WORKDIR /root/
 
-# EXPOSE 80
+EXPOSE 80
 
 ENTRYPOINT ["bash", "jsaispas.sh"]
--- srcs/jsaispas.sh
+++ srcs/jsaispas.sh
@@ -6,23 +6,32 @@
 #    By: ejahan <ejahan@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/17 15:46:54 by ejahan            #+#    #+#              #
-#    Updated: 2021/04/19 17:31:38 by ejahan           ###   ########.fr        #
+#    Updated: 2021/04/21 12:15:18 by ejahan           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 service nginx start
-mkdir /var/www/localhost
-cp localhost_index_on /etc/nginx/sites-available/localhost
-ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
+sleep infinity
 
+# mkdir var/www/localhost
+# mv ./default etc/nginx/sites-available
+# ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
+# chown -R www-data /var/www/*
+# chmod -R 755 /var/www/*
 
-wget http://fr.wordpress.org/latest-fr_FR.tar.gz
-tar -xzvf latest-fr_FR.tar.gz
-cp -r wordpress /var/www/localhost/wordpress
+ssl_certificate /etc/nginx/ssl/localhost.pem;
+ssl_certificate_key /etc/nginx/ssl/localhost.key;
 
+service php7.3-fpm start
+
+# mkdir /var/www/localhost
+# cp localhost_index_on /etc/nginx/sites-available/localhost
+# ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
+
+# wget http://fr.wordpress.org/latest-fr_FR.tar.gz
+# tar -xzvf latest-fr_FR.tar.gz
+# cp -r wordpress /var/www/localhost/wordpress
 
 bash
-
-
 
 # wget http://fr.wordpress.org/latest-fr_FR.tar.gz
